80eecce7c23ad3fd5fbafbee865c7ee2ba30b35f
Andrew Bell <andrewdavidbell@users.noreply.github.com>
2023-02-02 16:35:25 +1100

    Squashed 'macos-infra2/' content from commit 0baf2da
    git-subtree-dir: macos-infra2
    git-subtree-split: 0baf2da0052cba7d9aff2c076b1c0d714175d230

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ansible macOS provisioning
+[![Build Status](https://travis-ci.org/andrewdavidbell/macos-infra.svg?branch=generic-list-style)](https://travis-ci.org/andrewdavidbell/macos-infra)
+
+Ansible automation to configure macOS-based machines for software development
+and personal use.
---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+TRACE=${TRACE:-}
+[[ "$TRACE" ]] && set -x
+
+function install_homebrew() {
+    if [[ -x "$(command -v /opt/homebrew/bin/brew)" ]]
+    then
+        echo Homebrew already installed
+    else
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+        # Configure homebrew in this shell to allow the installation to proceed
+        # shellcheck disable=SC2016
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "${HOME}"/.zprofile
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+
+        # Turn off analytics
+        brew analytics off
+    fi
+}
+
+function install_ansible() {
+    if brew ls --versions ansible > /dev/null 2>&1
+    then
+        echo Homebrew ansible formula already installed
+    else
+        brew install ansible
+    fi
+}
+
+#
+# Create a macOS keychain entry and password lookup file for the Ansible Vault password
+#
+function install_ansible_vault_password() {
+    local ACCOUNT_NAME="macos-infra"
+    local SERVICE="Ansible Vault"
+
+    if /usr/bin/security find-generic-password -a "$ACCOUNT_NAME" -s "$SERVICE" > /dev/null 2>&1
+    then
+        echo Keychain entry for Ansible Vault password already installed
+    else
+        /usr/bin/security add-generic-password -a "$ACCOUNT_NAME" -s "$SERVICE" -w
+    fi
+
+    local VAULT_PASSWORD_FILE="./vault_password_file"
+
+    if [[ ! -e "$VAULT_PASSWORD_FILE" ]]
+    then
+	cat << EOF > "$VAULT_PASSWORD_FILE"
+#!/usr/bin/env bash
+
+/usr/bin/security find-generic-password -a "$ACCOUNT_NAME" -s "$SERVICE" -w
+EOF
+
+        chmod +x "$VAULT_PASSWORD_FILE"
+    fi
+}
+
+function main() {
+    install_homebrew
+    install_ansible
+
+    git clone https://github.com/andrewdavidbell/macos-infra.git
+    cd ./macos-infra
+
+    # install_ansible_vault_password
+    echo "Copy sensitive data into staging area"
+    echo "Then run:"
+    echo "ansible-playbook -K -i <home|work|client> site.yml -v"
+}
+
+main
